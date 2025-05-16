36e1506f594b35b1c22836aa8860ce438acccba9
Chasmical <kaz@chsm.dev>
2025-05-16 07:38:12 +0700

    ✨ Add stress type conversions, add stress! macro

--- crates/grammar_russian/src/declension/stresses.rs
+++ crates/grammar_russian/src/declension/stresses.rs
@@ -185,10 +185,20 @@ impl AnyDualStress {
         Self { main, alt }
     }
 }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum AdjectiveStressError {
+    Full(AdjectiveFullStressError),
+    Short(AdjectiveShortStressError),
+}
 impl AdjectiveStress {
     pub const fn new(full: AdjectiveFullStress, short: AdjectiveShortStress) -> Self {
         Self { full, short }
     }
+}
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum VerbStressError {
+    Present(VerbPresentStressError),
+    Past(VerbPastStressError),
 }
 impl VerbStress {
     pub const fn new(present: VerbPresentStress, past: VerbPastStress) -> Self {
@@ -196,9 +206,56 @@ impl VerbStress {
     }
 }
 
+//                         TABLE OF STRESS TYPE CONVERSIONS
+// ┌———————┬——————┬——————┬——————┬——————┬——————┬——————┬——————╥——————┬——————┬——————┐
+// │From\To│ Any  │ Noun │ AdjF │ AdjS │ Pro  │ VerbF│ VerbP║ ANY  │ ADJ  │ VERB │
+// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
+// │ Any   │ ———— │  []  │  []  │  []  │  []  │  []  │  []  ║  ██  │  []  │  []  │
+// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
+// │ Noun  │  ██  │ ———— │      │      │      │      │      ║  ██  │      │      │
+// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
+// │ AdjF  │  ██  │      │ ———— │      │      │      │      ║  ██  │  ██  │      │
+// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
+// │ AdjS  │  ██  │      │      │ ———— │      │      │      ║  ██  │  []  │      │
+// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
+// │ Pro   │  ██  │      │      │      │ ———— │      │      ║  ██  │      │      │
+// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
+// │ VerbF │  ██  │      │      │      │      │ ———— │      ║  ██  │      │  ██  │
+// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
+// │ VerbP │  ██  │      │      │      │      │      │ ———— ║  ██  │      │  []  │
+// ╞═══════╪══════╪══════╪══════╪══════╪══════╪══════╪══════╬══════╪══════╪══════╡
+// │ ANY   │  []  │      │      │      │      │      │      ║ ———— │  []  │  []  │
+// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
+// │ ADJ   │      │      │      │      │      │      │      ║  ██  │ ———— │      │
+// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
+// │ VERB  │      │      │      │      │      │      │      ║  ██  │      │ ———— │
+// └———————┴——————┴——————┴——————┴——————┴——————┴——————┴——————╨——————┴——————┴——————┘
+//                                                     ██ — From   [] — TryFrom
+
 impl<T: Into<AnyStress>> From<T> for AnyDualStress {
     fn from(value: T) -> Self {
         Self::new(value.into(), None)
+    }
+}
+impl TryFrom<AnyDualStress> for AnyStress {
+    type Error = ();
+    fn try_from(value: AnyDualStress) -> Result<Self, Self::Error> {
+        if value.alt.is_none() {
+            Ok(value.main)
+        } else {
+            Err(())
+        }
+    }
+}
+
+impl From<AdjectiveStress> for AnyDualStress {
+    fn from(value: AdjectiveStress) -> Self {
+        Self::new(value.full.into(), Some(value.short.into()))
+    }
+}
+impl From<VerbStress> for AnyDualStress {
+    fn from(value: VerbStress) -> Self {
+        Self::new(value.present.into(), Some(value.past.into()))
     }
 }
 
@@ -213,51 +270,117 @@ impl From<AdjectiveFullStress> for AdjectiveStress {
 impl TryFrom<AdjectiveShortStress> for AdjectiveStress {
     type Error = AdjectiveFullStressError;
     fn try_from(value: AdjectiveShortStress) -> Result<Self, Self::Error> {
+        Ok(Self::new(AnyStress::from(value).try_into()?, value))
+    }
+}
+impl TryFrom<AnyStress> for AdjectiveStress {
+    type Error = AdjectiveFullStressError;
+    fn try_from(value: AnyStress) -> Result<Self, Self::Error> {
+        AdjectiveShortStress::try_from(value).map_or(Err(Self::Error {}), |x| x.try_into())
+    }
+}
+
+impl From<VerbPresentStress> for VerbStress {
+    fn from(value: VerbPresentStress) -> Self {
+        Self::new(value, VerbPastStress::A)
+    }
+}
+impl TryFrom<VerbPastStress> for VerbStress {
+    type Error = VerbPresentStressError;
+    fn try_from(value: VerbPastStress) -> Result<Self, Self::Error> {
+        Ok(Self::new(AnyStress::from(value).try_into()?, value))
+    }
+}
+impl TryFrom<AnyStress> for VerbStress {
+    type Error = VerbPresentStressError;
+    fn try_from(value: AnyStress) -> Result<Self, Self::Error> {
+        VerbPastStress::try_from(value).map_or(Err(Self::Error {}), |x| x.try_into())
+    }
+}
+
+impl From<AdjectiveFullStressError> for AdjectiveStressError {
+    fn from(value: AdjectiveFullStressError) -> Self {
+        Self::Full(value)
+    }
+}
+impl From<AdjectiveShortStressError> for AdjectiveStressError {
+    fn from(value: AdjectiveShortStressError) -> Self {
+        Self::Short(value)
+    }
+}
+impl TryFrom<AnyDualStress> for AdjectiveStress {
+    type Error = AdjectiveStressError;
+    fn try_from(value: AnyDualStress) -> Result<Self, Self::Error> {
+        if let Some(alt) = value.alt {
+            Ok(Self::new(value.main.try_into()?, alt.try_into()?))
+        } else {
+            let alt = value.main.try_into()?;
+            Ok(Self::new(AnyStress::from(alt).try_into()?, alt))
+        }
+    }
+}
+
+impl From<VerbPresentStressError> for VerbStressError {
+    fn from(value: VerbPresentStressError) -> Self {
+        Self::Present(value)
+    }
+}
+impl From<VerbPastStressError> for VerbStressError {
+    fn from(value: VerbPastStressError) -> Self {
+        Self::Past(value)
+    }
+}
+impl TryFrom<AnyDualStress> for VerbStress {
+    type Error = VerbStressError;
+    fn try_from(value: AnyDualStress) -> Result<Self, Self::Error> {
         Ok(Self::new(
-            match value {
-                AdjectiveShortStress::A => AdjectiveFullStress::A,
-                AdjectiveShortStress::B => AdjectiveFullStress::B,
-                AdjectiveShortStress::Ap => AdjectiveFullStress::A,
-                AdjectiveShortStress::Bp => AdjectiveFullStress::B,
-                _ => return Err(Self::Error {}),
-            },
-            value,
+            value.main.try_into()?,
+            value.alt.map_or(Ok(VerbPastStress::A), |x| x.try_into())?,
         ))
     }
 }
 
-// TODO: more conversions
-// ┌———————┬——————┬——————┬——————┬——————┬——————┬——————┬——————╥——————┬——————┬——————┐
-// │From\To│ Any  │ Noun │ AdjF │ AdjS │ Pro  │ VerbF│ VerbP║ ANY  │ ADJ  │ VERB │
-// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
-// │ Any   │ ———— │  []  │  []  │  []  │  []  │  []  │  []  ║  ██  │      │      │
-// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
-// │ Noun  │  ██  │ ———— │      │      │      │      │      ║  ██  │      │      │
-// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
-// │ AdjF  │  ██  │      │ ———— │      │      │      │      ║  ██  │ ██   │      │
-// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
-// │ AdjS  │  ██  │      │      │ ———— │      │      │      ║  ██  │ []   │      │
-// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
-// │ Pro   │  ██  │      │      │      │ ———— │      │      ║  ██  │      │      │
-// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
-// │ VerbF │  ██  │      │      │      │      │ ———— │      ║  ██  │      │      │
-// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
-// │ VerbP │  ██  │      │      │      │      │      │ ———— ║  ██  │      │      │
-// ╞═══════╪══════╪══════╪══════╪══════╪══════╪══════╪══════╬══════╪══════╪══════╡
-// │ ANY   │      │      │      │      │      │      │      ║      │      │      │
-// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
-// │ ADJ   │      │      │      │      │      │      │      ║      │      │      │
-// ├———————┼——————┼——————┼——————┼——————┼——————┼——————┼——————╫——————┼——————┼——————┤
-// │ VERB  │      │      │      │      │      │      │      ║      │      │      │
-// └———————┴——————┴——————┴——————┴——————┴——————┴——————┴——————╨——————┴——————┴——————┘
-//                                                     ██ — From   [] — TryFrom
+#[macro_export]
+macro_rules! stress {
+    ($x:ident) => ({
+        stress![@res $x].try_into().unwrap()
+    });
+    ($x:ident/$y:ident) => ({
+        AnyDualStress::new(stress![@res $x], Some(stress![@res $y])).try_into().unwrap()
+    });
+    (@res $x:ident) => (
+        #[allow(non_upper_case_globals)]
+        {
+            const a: AnyStress = AnyStress::A;
+            const b: AnyStress = AnyStress::B;
+            const c: AnyStress = AnyStress::C;
+            const d: AnyStress = AnyStress::D;
+            const e: AnyStress = AnyStress::E;
+            const f: AnyStress = AnyStress::F;
+            const a1: AnyStress = AnyStress::Ap;
+            const b1: AnyStress = AnyStress::Bp;
+            const c1: AnyStress = AnyStress::Cp;
+            const d1: AnyStress = AnyStress::Dp;
+            const e1: AnyStress = AnyStress::Ep;
+            const f1: AnyStress = AnyStress::Fp;
+            const c2: AnyStress = AnyStress::Cpp;
+            const f2: AnyStress = AnyStress::Fpp;
+            $x
+        }
+    );
+}
 
-fn TEST() {
+#[allow(unused_variables, dead_code)]
+fn test_conversions() {
+    // TODO: make into a proper test
+
+    let i: NounStress = stress![a];
+    let j: AdjectiveStress = stress![a1];
+    let k: VerbStress = stress![b / c2];
+    let l: AdjectiveStress = stress![f2 / f2]; // TODO: should panic
+
     let x = NounStress::A;
     let y: AnyDualStress = x.into(); // should be a/
-
-    let x = NounStress::A;
-    let y: AdjectiveFullStress = x.into();
 
     let a = AdjectiveFullStress::A;
     let b: AdjectiveStress = a.into(); // should be a/a
@@ -268,5 +391,5 @@ fn TEST() {
     let v: AdjectiveStress = u.try_into().unwrap(); // should be b/b'
 
     let z = AnyDualStress::new(AnyStress::B, None);
-    let w: AdjectiveStress = z.into(); // should be b/b
+    let w: AdjectiveStress = z.try_into().unwrap(); // should be b/b
 }
