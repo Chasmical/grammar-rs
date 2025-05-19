f328bfeb8f77a3be1edb8aed537928d733167929
Chasmical <kaz@chsm.dev>
2025-05-19 13:54:53 +0700

    🚚 Move const traits, reduce vis to pub(crate)

---
+++ crates/grammar_russian/src/util/const_traits.rs
@@ -0,0 +1,78 @@
+// FIXME(const-hack): Remove these when [Try]From/Into are constified.
+
+#[const_trait]
+pub(crate) trait _From<T>: Sized {
+    fn _from(value: T) -> Self;
+}
+#[const_trait]
+pub(crate) trait _TryFrom<T>: Sized {
+    type Error;
+    fn _try_from(value: T) -> Result<Self, Self::Error>;
+}
+#[const_trait]
+pub(crate) trait _Into<T>: Sized {
+    fn _into(self) -> T;
+}
+#[const_trait]
+pub(crate) trait _TryInto<T>: Sized {
+    type Error;
+    fn _try_into(self) -> Result<T, Self::Error>;
+}
+
+impl<T, U: ~const _From<T>> const _Into<U> for T {
+    fn _into(self) -> U {
+        U::_from(self)
+    }
+}
+impl<T, U: ~const _TryFrom<T>> const _TryInto<U> for T {
+    type Error = U::Error;
+    fn _try_into(self) -> Result<U, Self::Error> {
+        U::_try_from(self)
+    }
+}
+impl<T, U: ~const _Into<T>> const _TryFrom<U> for T {
+    type Error = std::convert::Infallible;
+    fn _try_from(value: U) -> Result<Self, Self::Error> {
+        Ok(U::_into(value))
+    }
+}
+
+macro_rules! impl_const_From {
+    (
+        <$a:ty> for $b:ty {
+            $(#[$outer:meta])*
+            fn from($param:ident: $a2:ty) -> Self $body:block
+        }
+    ) => (
+        impl From<$a> for $b {
+            $(#[$outer])*
+            fn from($param: $a2) -> Self $body
+        }
+        impl const $crate::util::_From<$a> for $b {
+            $(#[$outer])*
+            fn _from($param: $a2) -> Self $body
+        }
+    );
+}
+macro_rules! impl_const_TryFrom {
+    (
+        <$a:ty> for $b:ty {
+            type Error = $err:ty;
+            $(#[$outer:meta])*
+            fn try_from($param:ident: $a2:ty) -> Result<Self, Self::Error> $body:block
+        }
+    ) => (
+        impl TryFrom<$a> for $b {
+            type Error = $err;
+            $(#[$outer])*
+            fn try_from($param: $a2) -> Result<Self, Self::Error> $body
+        }
+        impl const $crate::util::_TryFrom<$a> for $b {
+            type Error = $err;
+            $(#[$outer])*
+            fn _try_from($param: $a2) -> Result<Self, Self::Error> $body
+        }
+    );
+}
+
+pub(crate) use {impl_const_From, impl_const_TryFrom};
--- crates/grammar_russian/src/util/mod.rs
+++ crates/grammar_russian/src/util/mod.rs
@@ -1,33 +1,5 @@
-#[const_trait]
-pub trait _From<T>: Sized {
-    fn _from(value: T) -> Self;
-}
-#[const_trait]
-pub trait _TryFrom<T>: Sized {
-    type Error;
-    fn _try_from(value: T) -> Result<Self, Self::Error>;
-}
-#[const_trait]
-pub trait _Into<T>: Sized {
-    fn _into(self) -> T;
-}
-#[const_trait]
-pub trait _TryInto<T>: Sized {
-    type Error;
-    fn _try_into(self) -> Result<T, Self::Error>;
-}
-
-impl<T, U: ~const _From<T>> const _Into<U> for T {
-    fn _into(self) -> U {
-        U::_from(self)
-    }
-}
-impl<T, U: ~const _TryFrom<T>> const _TryInto<U> for T {
-    type Error = U::Error;
-    fn _try_into(self) -> Result<U, Self::Error> {
-        U::_try_from(self)
-    }
-}
+pub(crate) mod const_traits;
+pub(crate) use const_traits::*;
 
 macro_rules! enum_conversion {
     (
@@ -35,31 +7,14 @@ macro_rules! enum_conversion {
             $($variant:ident,)*
         }
     ) => (
-        impl const $crate::util::_From<$a> for $b {
-            fn _from(value: $a) -> Self {
-                match value {
-                    $(<$a>::$variant => Self::$variant,)*
-                }
-            }
-        }
-        impl From<$a> for $b {
+        $crate::util::impl_const_From!(<$a> for $b {
             fn from(value: $a) -> Self {
                 match value {
                     $(<$a>::$variant => Self::$variant,)*
                 }
             }
-        }
-        impl const $crate::util::_TryFrom<$b> for $a {
-            type Error = $err;
-            #[allow(unreachable_patterns)]
-            fn _try_from(value: $b) -> Result<Self, Self::Error> {
-                Ok(match value {
-                    $( <$b>::$variant => Self::$variant, )*
-                    _ => return Err(Self::Error {}),
-                })
-            }
-        }
-        impl TryFrom<$b> for $a {
+        });
+        $crate::util::impl_const_TryFrom!(<$b> for $a {
             type Error = $err;
             #[allow(unreachable_patterns)]
             fn try_from(value: $b) -> Result<Self, Self::Error> {
@@ -68,7 +23,7 @@ macro_rules! enum_conversion {
                     _ => return Err(Self::Error {}),
                 })
             }
-        }
+        });
     );
 }
 
