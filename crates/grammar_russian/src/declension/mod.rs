e84a185ebfc2cd63935fe63793adf53fc2b8327b
Chasmical <kaz@chsm.dev>
2025-05-22 20:06:09 +0700

    🚚 Move endings data to one file

--- crates/grammar_russian/src/declension/mod.rs
+++ crates/grammar_russian/src/declension/mod.rs
@@ -1,15 +1,13 @@
 mod defs;
-mod stem_types;
 mod endings;
-mod endings_data;
+mod stem_types;
 mod impl_adjective;
 mod impl_noun;
 mod impl_pronoun;
 
 pub use defs::*;
-pub use stem_types::*;
 pub use endings::*;
-pub use endings_data::*;
+pub use stem_types::*;
 pub use impl_adjective::*;
 pub use impl_noun::*;
 pub use impl_pronoun::*;
